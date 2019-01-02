b5abab10df1c9ba46ff2c262916770636770a324
radiansz <lightwaveinthespace@gmail.com>
2019-01-02 12:22:10 +0300

    Add trials to db, add date fields to db, add stats fields

--- src/shared/database/initializeDatabase.js
+++ src/shared/database/initializeDatabase.js
@@ -1,21 +1,44 @@
 function createTables(transaction) {
-    // transaction.executeSql(`DROP TABLE IF EXISTS Deck;`);
-    // transaction.executeSql(`DROP TABLE IF EXISTS Card;`);
+    // transaction.executeSql(`DROP TABLE IF EXISTS Decks;`);
+    // transaction.executeSql(`DROP TABLE IF EXISTS Cards;`);
+    // transaction.executeSql(`DROP TABLE IF EXISTS Trials;`);
     // transaction.executeSql(`DROP TABLE IF EXISTS Version;`);
 
   transaction.executeSql(`
-    CREATE TABLE IF NOT EXISTS Deck( 
+    CREATE TABLE IF NOT EXISTS Decks( 
       deckId INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT
+      name TEXT,
+      createdAt INTEGER DEFAULT 0,
+      updatedAt INTEGER DEFAULT 0
     );
   `);
 
   transaction.executeSql(`
-    CREATE TABLE IF NOT EXISTS Card(  
+    CREATE TABLE IF NOT EXISTS Cards(  
       cardId INTEGER PRIMARY KEY AUTOINCREMENT,  
       deckId INTEGER,  
       frontside TEXT,  
-      backside TEXT
+      backside TEXT,
+      numberOfViews INTEGER DEFAULT 0,
+      numberOfMatches INTEGER DEFAULT 0,
+      createdAt INTEGER DEFAULT 0,
+      updatedAt INTEGER DEFAULT 0
+
+      REFERENCES Decks (deckId)
+    );
+  `);
+
+  transaction.executeSql(`
+    CREATE TABLE IF NOT EXISTS Trials(  
+      trialId INTEGER PRIMARY KEY AUTOINCREMENT, 
+      deckId INTEGER, 
+      type TEXT DEFAULT 'default',  
+      numberOfCards INTEGER,
+      numberOfViewedCards INTEGER,
+      numberOfMatchedCards INTEGER,
+      createdAt INTEGER DEFAULT 0 
+
+      REFERENCES Decks (deckId)
     );
   `);
 
--- src/shared/database/queryCreators.js
+++ src/shared/database/queryCreators.js
@@ -3,29 +3,56 @@ export function createCardInDeck(deckId, {
   backside,
 }) {
   return {
-    query: `INSERT INTO Card (deckId, frontside, backside) VALUES (?, ?, ?)`,
-    args: [deckId, frontside, backside],
-  };
-}
-
-export function getCardsFromDeck(deckId) {
-  return {
-    query: `SELECT cardId, frontside, backside FROM Card WHERE deckId = ?`,
-    args: [deckId],
+    query: `INSERT INTO 
+      Cards (deckId, frontside, backside, createdAt, updatedAt) 
+      VALUES (?, ?, ?, ?, ?)
+    `,
+    args: [deckId, frontside, backside, new Date().getTime(), new Date().getTime()],
   };
 }
 
 export function createDeck(name) {
   return {
-    query: `INSERT INTO Deck (name) VALUES (?)`,
-    args: [name],
+    query: `INSERT INTO 
+      Decks (name, createdAt, updatedAt) 
+      VALUES (?, ?, ?)
+    `,
+    args: [name, new Date().getTime(), new Date().getTime()],
+  };
+}
+
+export function getCardsFromDeck(deckId) {
+  return {
+    query: `SELECT cardId, frontside, backside FROM Cards WHERE deckId = ?`,
+    args: [deckId],
   };
 }
 
 export function getDecks() {
   return {
     query: `
-      SELECT deckId, name FROM Deck
+      SELECT 
+        deckId, 
+        name,
+        (SELECT COUNT(*) FROM Cards WHERE Cards.deckId = Decks.deckId) AS numberOfCards 
+      FROM Decks
     `,
+  }
+};
+
+export function getDeckStats(deckId) {
+  return {
+    query: `
+      SELECT 
+        deckId, 
+        name,
+        (SELECT COUNT(*) FROM Cards WHERE Cards.deckId = Decks.deckId) AS numberOfCards,
+        (SELECT COUNT(*) FROM Trials WHERE Trials.deckId = Decks.deckId) AS numberOfTrials,
+        (SELECT COUNT(*) FROM Cards WHERE Cards.deckId = Decks.deckId AND Cards.numberOfViews > 10) AS numberOfLearnedCards
+      FROM Decks
+      WHERE
+        deckId = ?
+    `,
+    args: [deckId]
   }
 }
