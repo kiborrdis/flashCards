ce45c559b99676c6cff4c953338f86905c7d4d85
radiansz <lightwaveinthespace@gmail.com>
2019-06-24 23:31:43 +0300

    Upgrade react native

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import startApp from './startApp';
+import React, { Component } from 'react';
+import { AppRegistry, Image } from 'react-native';
 
-startApp();
+export default class Bananas extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <Image source={pic} style={{width: 193, height: 110}}/>
+    );
+  }
+}
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => Bananas);
